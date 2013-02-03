8424be197a7675b095e036ab444f3e516c618d6a
Sarah Allen <sarah@ultrasaurus.com>
2013-02-03 15:01:09 -0800

    remove unused text field

--- FunCollection/FunViewController.h
+++ FunCollection/FunViewController.h
@@ -18,8 +18,6 @@ typedef LayoutStyle;
 @interface FunViewController : UIViewController
 - (IBAction)switchLayout:(id)sender;
 
-@property (weak, nonatomic) IBOutlet UITextField *layoutNameField;
-
 @property (nonatomic, assign, readonly) LayoutStyle layoutStyle;
 
 @end
