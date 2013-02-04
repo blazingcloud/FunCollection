5f2c243cf313aef11b284f3e6e8be1e58f9aac09
Sarah Allen <sarah@ultrasaurus.com>
2013-02-04 11:56:23 -0800

    assign copyright to Blazing Cloud, Inc.

--- FunCollection/GridLayout.h
+++ FunCollection/GridLayout.h
@@ -3,7 +3,7 @@
 //  FunCollection
 //
 //  Created by Sarah Allen on 2/3/13.
-//  Copyright (c) 2013 Sarah Allen. All rights reserved.
+//  Copyright (c) 2013 Blazing Cloud, Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
